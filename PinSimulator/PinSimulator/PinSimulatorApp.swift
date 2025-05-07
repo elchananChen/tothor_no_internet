ce3177ff12e40b2ab7f76d3498fc378f3e9f603b
elchanan chen <elchananc79@gmail.com>
2025-05-07 14:49:54 +0300

    f

---
+++ PinSimulator/PinSimulator/PinSimulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinSimulatorApp.swift
+//  PinSimulator
+//
+//  Created by elchanan chen on 07/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct PinSimulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PinSimulator/PinSimulatorTests/PinSimulatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  PinSimulatorTests.swift
+//  PinSimulatorTests
+//
+//  Created by elchanan chen on 07/05/2025.
+//
+
+import Testing
+@testable import PinSimulator
+
+struct PinSimulatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
